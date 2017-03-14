ea38ce1db04e43538a487b27020603fb95e6b61c
Nphillip <nyalphillip@gmail.com>
2017-03-14 13:58:22 -0400

    Test Files

---
+++ Customlogin/Customlogin/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Customlogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Once I created a second Form called Main I called this. It will allow me to click on the exit and login button\\
+            
+            this.Hide();
+            Main gg = new Main();
+            gg.Show();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //This will allow me to close the windows.\\
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ calc/calc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count;
+            Console.WriteLine("choose your function");
+            Console.WriteLine("1. * \n2. - \n3. /  \n4. + \n5. %");
+            string user = Console.ReadLine();
+            if (int.TryParse(user , out count)) {
+                switch (count) {
+                    case 1:
+                        Console.WriteLine("input first: ");
+                        string first = Console.ReadLine();
+                        Console.WriteLine("input second: ");
+                        string second = Console.ReadLine();
+                        double total;
+                        total = Convert.ToInt32(first) * Convert.ToInt32(second);
+                        Console.WriteLine("output: ");
+                        Console.WriteLine(total);
+                        Console.Read();
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("input first: ");
+                        string third = Console.ReadLine();
+                        Console.WriteLine("input second: ");
+                        string fourth = Console.ReadLine();
+                        double total;
+                        total = Convert.ToInt32(third) - Convert.ToInt32(fourth);
+                        Console.WriteLine("output: ");
+                        Console.WriteLine(total);
+                        Console.Read();
+
+                        break;
+
+                    case 3:
+                        Console.WriteLine("input first: ");
+                        string first = Console.ReadLine();
+                        Console.WriteLine("input second: ");
+                        string second = Console.ReadLine();
+                        double total;
+                        total = Convert.ToInt32(first) / Convert.ToInt32(second);
+                        Console.WriteLine("output: ");
+                        Console.WriteLine(total);
+                        Console.Read();
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine("input first: ");
+                        string first = Console.ReadLine();
+                        Console.WriteLine("input second: ");
+                        string second = Console.ReadLine();
+                        double total;
+                        total = Convert.ToInt32(first) + Convert.ToInt32(second);
+                        Console.WriteLine("output: ");
+                        Console.WriteLine(total);
+                        Console.Read();
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine("input first: ");
+                        string first = Console.ReadLine();
+                        Console.WriteLine("input second: ");
+                        string second = Console.ReadLine();
+                        double total;
+                        total = Convert.ToInt32(first) % Convert.ToInt32(second);
+                        Console.WriteLine("output: ");
+                        Console.WriteLine(total);
+                        Console.Read();
+
+                        break;
+
+                }
+            }
+            else {
+            }
+
+        }           
+        
+    }
+}
